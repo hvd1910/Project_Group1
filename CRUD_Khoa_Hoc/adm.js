8a71cc83ae903c3ae26bcafcb39b8cd97814d92a
Hồ Việt Đức <duchvth2207073@fpt.edu.vn>
2023-07-28 08:18:47 +0700

    up
    up

--- CRUD_Khoa_Hoc/adm.js
+++
@@ -1,5 +0,0 @@
-var navsystem = document.querySelector('.navsystem');
-var systemlist = document.querySelector('.nav_system-list');
-navsystem.onclick = function () {
-    systemlist.classList.toggle('open_sytem');
-}
